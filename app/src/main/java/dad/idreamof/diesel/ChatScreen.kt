a9523573468eca9affeab60a9a9ca342d8872f80
Diesel Benzyna <diesel@idreamof.dad>
2026-05-21 13:49:27 -0700

    refactor(chat): add missing compose runtime imports

--- app/src/main/java/dad/idreamof/diesel/ChatScreen.kt
+++ app/src/main/java/dad/idreamof/diesel/ChatScreen.kt
@@ -52,9 +52,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
